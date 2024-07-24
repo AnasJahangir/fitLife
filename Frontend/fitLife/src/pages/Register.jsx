7bfc090d762198b72590e9d2199644ba72f28c82
Anas Jahangir <100097449+samiullah07@users.noreply.github.com>
2024-07-24 05:27:29 +0500

    d

--- Frontend/fitLife/src/pages/Register.jsx
+++ Frontend/fitLife/src/pages/Register.jsx
@@ -164,7 +164,7 @@ const RegisterForm = () => {
 
               <button
                 type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-black dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="w-full text-white bg-black hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-black dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Create an account
               </button>
